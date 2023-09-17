d81658fe69a2a293cc75ad0823bf3f24a37fc1ca
andygao82 <freedomandy82@gmail.com>
2023-09-17 14:29:11 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("hello world!");
+var dou = "Angela Gao";
+var niu = "Daniel Gao";
+console.log(dou + " and " + niu);
---
+++ index.ts
@@ -0,0 +1,4 @@
+const dou: string = "Angela Gao";
+const niu: string = "Daniel Gao";
+const gao: string = "Surname"
+console.log(dou + " and " + niu)
